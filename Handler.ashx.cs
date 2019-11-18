d95d783b106caaf4844e9410c62b6017804133c1
alejandrio.vasay <alejandrio.vasay@tccd.edu>
2019-11-18 08:39:07 -0600

    initial commit

---
+++ Handler.ashx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace GetImageHandler
+{
+    /// <summary>
+    /// Summary description for Handler
+    /// </summary>
+    public class Handler : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.Cache.SetCacheability(HttpCacheability.Public);
+            context.Response.ContentType = "image/jpeg";
+            string rootDir = System.Web.Hosting.HostingEnvironment.ApplicationPhysicalPath;
+
+            string imagePath = Path.Combine(rootDir, @"images\biology\biology-2.jpg");
+            
+            byte[] byteArray = File.ReadAllBytes(imagePath);
+            context.Response.BinaryWrite(byteArray);
+
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
